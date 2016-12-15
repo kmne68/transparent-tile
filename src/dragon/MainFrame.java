6110a40c0ac1f543d34c532a437604e6f79d21fe
kmne68 <kmne68@gmail.com>
2016-12-14 23:34:05 -0600

    Add MainFrame class (WIP)

---
+++ src/dragon/MainFrame.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dragon;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author Keith
+ */
+public class MainFrame extends JFrame implements ActionListener {
+    
+    private Timer timer;
+    long startTime;
+    long relativeTime;
+    private Game game;
+    private StartPanel startPanel;
+    private CardLayout card;
+    
+    MainFrame() {
+        super("Dragon");
+        
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MotifLookAndFeel");
+        } catch (Exception e) {
+            System.out.println("Cannot set look and feel");
+        }
+        
+        cards = new CardLayout();
+        startPanel = new StartPanel();
+        game = new Game();
+        
+        startPanel.setListener(new StartPanelListener() {
+            
+            public void startGame() {
+                
+                card.show(MainFrame.this.getContentPane(), "game");
+            }
+        });
+        
+        timer = new Timer(20, this);
+        timer.setInialDelay(100);
+        
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        
+        game.update();
+    }
+    
+    
+}
